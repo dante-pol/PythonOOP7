f273a42672e49885c9caed3cf86b26c6d6ba5d20
Dmitry Rak <dmitry.rak@seschool.ru>
2023-08-10 21:05:04 +0300

    Added entityes for zoo.

---
+++ SimpleClass/zoo.py
@@ -0,0 +1,20 @@
+class Client:
+    def __init__(self, first_name, second_name, age):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.age = age
+
+
+class Animal:
+    def __init__(self, type, color, name):
+        self.name = name
+        self.type = type
+        self.color = color
+
+
+class Worker:
+    def __init__(self, name, visiting_day, visiting_time):
+        self.name = name
+        self.visiting_day = visiting_day
+        self.visiting_time = visiting_time
+
