07fe9f2233a532027efc11473a98a0883eeab1dd
Mirrabellko <burlutskayavalery@mail.ru>
2023-08-14 08:54:38 +0300

    Clients were added

--- SimpleClass/zoo.py
+++ SimpleClass/zoo.py
@@ -5,6 +5,13 @@ class Client:
         self.age = age
 
 
+Client_1 = Client("Masha", "Shishkina", 25)
+Client_2 = Client("Sasha", "Shishkin", 20)
+Client_3 = Client("Fedor", "Romanov", 40)
+Client_4 = Client("Elena", "Ivanova", 20)
+Client_5 = Client("Filip", "Fedorov", 35)
+
+
 class Animal:
     def __init__(self, type, color, name):
         self.name = name
